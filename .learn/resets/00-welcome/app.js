d11d513ea10269a332b90917bb6cade1cdd8c314
Neruoal <manuellareogarcia@gmail.com>
2026-03-03 22:03:51 +0000

    First commit

---
+++ .learn/resets/00-welcome/app.js
@@ -0,0 +1,15 @@
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+const fromEuroToYen = function()
+
+module.exports = { fromEuroToDollar }
---
+++ app.js
@@ -0,0 +1,23 @@
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInEuro) {
+    let valueInYen = valueInEuro * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInEuro) {
+    let valueInPound = valueInEuro * 0.87;
+    return valueInPound;
+}
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound }
